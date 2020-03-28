7388dfbb950f4872065d786b2461e426c670ae06
René Schramowski <kounex@Renes-iMac.fritz.box>
2020-03-28 04:08:51 +0100

    autodiscovery only works partial - there seems to be connection lacks or timeout problems whatever, need to wait for refresh to actually work

--- lib/utils/network_helper.dart
+++ lib/utils/network_helper.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:get_ip/get_ip.dart';
-import 'package:mobx/mobx.dart';
 import 'package:ping_discover_network/ping_discover_network.dart';
 import 'package:web_socket_channel/io.dart';
 
@@ -18,25 +17,37 @@ class NetworkHelper {
   static Future<List<String>> getAvailableOBSIPs(
       {Duration timeout = const Duration(seconds: 5)}) async {
     String baseIP = (await GetIp.ipAddress).split('.').take(3).join('.');
+    List<IOWebSocketChannel> connectedChannels = [];
     List<int> availableIPs = [];
 
-    List.generate(256, (index) {
-      IOWebSocketChannel channel =
-          IOWebSocketChannel.connect("ws://$baseIP.${index.toString()}:4444");
+    List.generate(256, (index) async {
+      IOWebSocketChannel channel = IOWebSocketChannel.connect(
+          "ws://$baseIP.${index.toString()}:4444",
+          pingInterval: Duration(milliseconds: 1000));
+      bool closed = false;
 
       channel.stream.listen(
         ((event) {
           print(event);
           availableIPs.add(index);
-          channel.sink.close();
+          connectedChannels.add(channel);
         }),
-        onError: (_) => channel.sink.close(),
+        onError: (_) => closed = true,
+        onDone: () => closed = true,
+        cancelOnError: true,
       );
 
-      channel.sink.add(json.encode('GetAuthRequired'));
+      await Future.delayed(Duration(milliseconds: 2500));
+
+      if (!closed) {
+        print('how?');
+        channel.sink.add(json.encode('GetAuthRequired'));
+      }
     });
 
     await Future.delayed(timeout);
+
+    await Future.wait(connectedChannels.map((c) => c.sink.close()));
 
     return availableIPs.map((i) => '$baseIP.$i').toList();
   }
