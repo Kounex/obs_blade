f632b0c14ca022eedc2ef91a7ceca76b3d06be83
Kounex <r.schramowski@googlemail.com>
2020-05-30 00:07:31 +0200

    format

--- lib/types/classes/api/scene.dart
+++ lib/types/classes/api/scene.dart
@@ -6,11 +6,15 @@ class Scene {
   String name;
   List<SceneItem> sources;
 
-  Scene({@required this.name, @required this.sources});
+  Scene({
+    @required this.name,
+    @required this.sources,
+  });
 
-  static Scene fromJSON(Map<String, dynamic> fullJSON) => Scene(
-      name: fullJSON['name'],
-      sources: (fullJSON['sources'] as List<dynamic>)
-          .map((source) => SceneItem.fromJSON(source))
-          .toList());
+  static Scene fromJSON(Map<String, dynamic> json) => Scene(
+        name: json['name'],
+        sources: (json['sources'] as List<dynamic>)
+            .map((source) => SceneItem.fromJSON(source))
+            .toList(),
+      );
 }
