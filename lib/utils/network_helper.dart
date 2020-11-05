93d75eb4dec04af61cf9da00abb3cc18d4581deb
Kounex <r.schramowski@googlemail.com>
2020-11-05 11:10:25 +0100

    added debug print for requests

--- lib/utils/network_helper.dart
+++ lib/utils/network_helper.dart
@@ -68,6 +68,7 @@ class NetworkHelper {
   /// sent back through the stream so we every listener can act accordingly
   static void makeRequest(IOWebSocketChannel channel, RequestType request,
       [Map<String, dynamic> fields]) {
+    print(request);
     channel.sink.add(json.encode({
       'message-id': request.index.toString(),
       'request-type': request.toString().split('.')[1],
