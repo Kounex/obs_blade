8282c189910e84fb04ed0280f79b6f5d21aa6c9e
Rene Schramowski <kounex@a79fb238-9ed7-4aeb-90f8-a5205110288e.fritz.box>
2021-09-27 17:36:57 +0200

    feature(ios-pro-motion): enabled pro motion support

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,17 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      
+      if #available(iOS 15.0, *) {
+          let displayLink = CADisplayLink(target: self, selector: #selector(step))
+          displayLink.preferredFrameRateRange = CAFrameRateRange(minimum:80, maximum:120, preferred:120)
+          displayLink.add(to: .current, forMode: .default)
+      }
+      
+      GeneratedPluginRegistrant.register(with: self)
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    @objc func step(displaylink: CADisplayLink) {
+    
+    }
 }
