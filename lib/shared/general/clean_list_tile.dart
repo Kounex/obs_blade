23a5f3fa07da729d84246f159c2999e8c5314677
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2023-02-15 23:33:19 -0800

    fix: theme button

--- lib/shared/general/clean_list_tile.dart
+++ lib/shared/general/clean_list_tile.dart
@@ -24,7 +24,7 @@ class CleanListTile extends StatelessWidget {
               Text(
                 this.title,
                 style:
-                    Theme.of(context).textTheme.button!.copyWith(fontSize: 16),
+                    Theme.of(context).textTheme.labelLarge!.copyWith(fontSize: 16),
               ),
               const SizedBox(height: 4.0),
               Text(
