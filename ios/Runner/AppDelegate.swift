d148db673798fd3bdfc5fbc8292a186c7f23ca92
Kounex <r.schramowski@googlemail.com>
2026-08-12 22:25:13 +0200

    ios: adopt UIScene lifecycle (scene manifest + implicit engine delegate)
    Apple will require UIScene lifecycle for apps built with the SDK
    following iOS 26; the CLI auto-migration skipped this app because
    AppDelegate counted as customized (dead ProMotion experiment code).
    
    - AppDelegate: conform to FlutterImplicitEngineDelegate, move plugin
      registration to didInitializeImplicitFlutterEngine, drop the dead
      didFinishLaunchingWithOptions override + CADisplayLink leftovers
    - Info.plist: add UIApplicationSceneManifest (single scene,
      FlutterSceneDelegate, Main storyboard)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,31 +2,8 @@ import UIKit
 import Flutter
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      
-      /**
-       Was initially added manually to enable ProMotion (partially)
-       Is now supported natively by Flutter
-       */
-      
-      // Check whether it's iOS 15.0 or above (since this API is only available there)
-//      if #available(iOS 15.0, *) {
-//          // Instance of CADisplayLink which exposes several frame rate related options
-//          let displayLink = CADisplayLink(target: self, selector: #selector(step))
-//          // Set the preferred range of frames (in this case "High-impact animations" based on the documentation)
-//          displayLink.preferredFrameRateRange = CAFrameRateRange(minimum:80, maximum:120, preferred:120)
-//          // Enable it by adding it to the main runloop
-//          displayLink.add(to: .current, forMode: .default)
-//      }
-      
-      GeneratedPluginRegistrant.register(with: self)
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
-    @objc func step(displaylink: CADisplayLink) {
-        // Will be called once a frame has been built while matching desired frame rate
-    }
 }
